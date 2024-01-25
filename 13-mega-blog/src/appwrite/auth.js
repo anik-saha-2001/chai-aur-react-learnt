d7c68b76daabe10ba467df8f45cb20818922718e
anik-saha-2001 <aniksahamda@gmail.com>
2024-01-25 18:12:27 +0530

    updated auth.js

--- 13-mega-blog/src/appwrite/auth.js
+++ 13-mega-blog/src/appwrite/auth.js
@@ -1,5 +1,7 @@
-import conf from "../conf/conf";
+/* eslint-disable no-useless-catch */
+
 import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
 
 export class AuthService {
   client = new Client();
@@ -11,6 +13,51 @@ export class AuthService {
       .setProject(conf.appwriteProjectID);
 
     this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method after success
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
   }
 }
 
