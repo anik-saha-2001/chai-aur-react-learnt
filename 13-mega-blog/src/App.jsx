70e04f2bf1a930515b670e0d509149fd05572580
anik-saha-2001 <aniksahamda@gmail.com>
2024-01-31 18:06:18 +0530

    video 22 watched

--- 13-mega-blog/src/App.jsx
+++ 13-mega-blog/src/App.jsx
@@ -1,17 +1,35 @@
-import conf from "./conf/conf";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import {Outlet} from "react-router-dom"
 
 function App() {
-  console.log(conf.appwriteUrl);
-  console.log(conf.appwriteBucketID);
-  console.log(conf.appwriteCollectionID);
-  console.log(conf.appwriteDatabaseID);
-  console.log(conf.appwriteProjectID);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
-    <>
-      <h1>A blog app with Appwirte</h1>
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-green-500">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {<Outlet />}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
